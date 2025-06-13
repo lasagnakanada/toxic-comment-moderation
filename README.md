cad351391c3b99aa4af809f4edd2e903592b1e6d
lasagnakanada <abathingrevetg@gmail.com>
2025-06-13 04:36:09 +0200

    mini-batch training

--- README.md
+++ README.md
@@ -14,5 +14,19 @@ we get more concrete information about how specifically each comment is toxic (i
 Most of the comments have significant disbalance in terms of its length, belonging to a specific class (there is only 5% of threat comments for example).
 This information is useful for future analysis and input limitations in our model.
 
+During investigation of our data I decided that F1-score is highly important metric for this kind of task where it is highly important to predict toxic comments (obviously) and still be able to precisely detect non-toxic ones. What I mean by this is that you can just mark all comments as toxic and get recall 100% but we still want to accept non-toxic comments in our moderation to provide an option for users to share their opinion with others. With such understanding of existing balance demand, F1-score is a highly reliable metric which can describe both: ability to detect 'niche' data and still getting high perfomance on usual data-traffic. 
+
+P.S.: The same logic applies to all tasks where it is highly important to detect low-frequently appearing event and still soberly deal with rest of the data. Paradox is that soon your main task becomes to properly detect 'rest of the data' and not those rarely appearing events since you can just always predict any event as 'rare' and get 100% recall. Think about medical diagnosis process, food for thought.
+
+
+input_size = 10000 (at this point we use TF-IDF to select 10k most frequently appearing words and describe them as a single vector where each dimension represents single weight of a particular word)
+
+hidden_size = 256 (in future this number might increase after replacing TF-IDF with something more complex but currently it is just important to move in direction of our aim)
+
+output_size = 6 (we have six classes to classify from, for each output unit we get a binary classifier which decides whether specific comment is toxic or not).
+
+
+
+
 
 
--- notebooks/pipeline.py
+++ notebooks/pipeline.py
@@ -9,6 +9,7 @@ from torch import cuda
 from torch.utils.data import DataLoader, TensorDataset
 import numpy as np
 import torch.nn as nn   
+from sklearn.preprocessing import StandardScaler
 
 # Path to the project directory
 # Change this to your project directory
@@ -28,7 +29,7 @@ class ToxicClassifier(nn.Module):
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size//2)
         self.fc3 = nn.Linear(hidden_size//2, output_size)    
-        self.dropout = nn.Dropout(0.4)
+        self.dropout = nn.Dropout(0.3)
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
         self.batch_norm1 = nn.BatchNorm1d(hidden_size)
@@ -38,6 +39,9 @@ class ToxicClassifier(nn.Module):
     def data_preprocessing(self, X: np.ndarray, y: np.ndarray) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         X_temp, X_test, y_temp, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
         X_train, X_val, y_train, y_val = train_test_split(X_temp, y_temp, test_size=0.1765, random_state=42)
+        scaler = StandardScaler()
+        X_train = scaler.fit_transform(X_train)
+        X_val = scaler.transform(X_val)
         self.X_train = torch.tensor(X_train, dtype=torch.float32)
         self.X_test = torch.tensor(X_test, dtype=torch.float32)
         self.y_train = torch.tensor(y_train, dtype=torch.float32)
@@ -56,7 +60,7 @@ class ToxicClassifier(nn.Module):
         return x
     
     def metrics(self, prediction: torch.Tensor, target: torch.Tensor) -> tuple[float, float, float, float]:
-        pred_labels = (prediction.detach().cpu().numpy() > 0.5).astype(int)
+        pred_labels = (prediction.detach().cpu().numpy() > 0.7).astype(int)
         target_np = target.detach().cpu().numpy().astype(int)
         accuracy = accuracy_score(target_np, pred_labels)
         recall = recall_score(target_np, pred_labels, average="macro", zero_division=0)
@@ -64,34 +68,52 @@ class ToxicClassifier(nn.Module):
         f1 = f1_score(target_np, pred_labels, average="macro", zero_division=0)
         return accuracy, recall, precision, f1
     
-    def train_model(self, X_train: torch.Tensor, y_train: torch.Tensor, X_val: torch.Tensor, y_val: torch.Tensor, epochs: int = 10, learning_rate: float = 0.01):
+    def train_model(self, X_train: torch.Tensor, y_train: torch.Tensor, X_val: torch.Tensor, y_val: torch.Tensor, epochs: int = 10, learning_rate: float = 0.0001):
         device = "cuda" if torch.cuda.is_available() else "cpu"
         self.to(device)
         X_train = X_train.to(device)
         y_train = y_train.to(device)
-        optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate, weight_decay=1e-5)
+        optimizer = torch.optim.AdamW(self.parameters(), lr=learning_rate, weight_decay=1e-5)
         pos_counts = y_train.sum(axis=0)
         neg_counts = y_train.shape[0] - pos_counts
         pos_weight = torch.tensor(neg_counts / pos_counts, dtype=torch.float32)
 
         loss_function = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
-        for epoch in range(epochs):
-            self.train()
-            optimizer.zero_grad()
-            prediction = self.forward(X_train)
-            loss = loss_function(prediction, y_train)
-            loss.backward()
-            optimizer.step()
-            train_metrics = self.metrics(prediction, y_train)
-            self.eval()
-            with torch.no_grad():
-                val_prediction = self.forward(X_val)
-                val_loss = loss_function(val_prediction, y_val)
-                val_metrics = self.metrics(val_prediction, y_val)
-            print(f'Epoch {epoch+1}, Train Loss: {loss.item()}, Validation Loss: {val_loss.item()},'
-            f'Train Metrics: {train_metrics}, Validation Metrics: {val_metrics}')
-        
 
+        best_val_loss = float('inf')
+        patience = 5
+        counter = 0
+        
+        for epoch in range(epochs):
+            for X_batch, y_batch in train_loader:
+                X_batch = X_batch.to(device)
+                y_batch = y_batch.to(device)
+
+                self.train()
+                optimizer.zero_grad()
+                prediction = self.forward(X_train)
+                loss = loss_function(prediction, y_train)
+                loss.backward()
+                optimizer.step()
+                train_metrics = self.metrics(prediction, y_train)
+                self.eval()
+                with torch.no_grad():
+                    val_prediction = self.forward(X_val)
+                    val_loss = loss_function(val_prediction, y_val)
+                    val_metrics = self.metrics(val_prediction, y_val)
+                print(f'Epoch {epoch+1}, Train Loss: {loss.item()}, Validation Loss: {val_loss.item()},'
+                f'Train Metrics: {train_metrics}, Validation Metrics: {val_metrics}')
+                if val_loss < best_val_loss:
+                    best_val_loss = val_loss
+                    counter = 0
+                else:
+                    counter += 1
+                    if counter >= patience:
+                        print(f'Early stopping!')
+                        break
+        
+        print("Train targets sum per class:", self.y_train.sum(dim=0))
+        print("Val targets sum per class:", self.y_val.sum(dim=0))
 
     def evaluate(self, X_test: torch.Tensor, y_test: torch.Tensor) -> tuple[float, float, float, float]:
         device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -105,7 +127,9 @@ class ToxicClassifier(nn.Module):
         print(f"Loss: {loss.item()}, Accuracy: {accuracy}, Recall: {recall}, Precision: {precision}, F1: {f1}")
 
 if __name__ == "__main__":
-    our_model = ToxicClassifier(input_size=10000, hidden_size=500, output_size=6)
+    our_model = ToxicClassifier(input_size=10000, hidden_size=256, output_size=6)
     our_model.data_preprocessing(X, y)
-    our_model.train_model(our_model.X_train, our_model.y_train, our_model.X_val, our_model.y_val, epochs=30, learning_rate=0.001)
+    train_loader = DataLoader(TensorDataset(our_model.X_train, our_model.y_train), batch_size=64, shuffle=True)
+    val_loader = DataLoader(TensorDataset(our_model.X_val, our_model.y_val), batch_size=64, shuffle=False)
+    our_model.train_model(our_model.X_train, our_model.y_train, our_model.X_val, our_model.y_val, epochs=30, learning_rate=0.0001)
     
